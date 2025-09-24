e33122bd3610e5a363e77168232c21162baad25a
Matthew Filipovich <42307495+MatthewFilipovich@users.noreply.github.com>
2025-09-23 18:54:39 -0700

    working with jax for loop!

--- pycharge/simulate/simulate.py
+++ pycharge/simulate/simulate.py
@@ -1,6 +1,5 @@
 import jax
 import jax.numpy as jnp
-from tqdm import tqdm
 
 from pycharge import Charge
 
@@ -84,20 +83,34 @@ def simulate(sources):
             other_charges = charge_set - {c for c in charge_list[source_idx]}
             func_ode_list.append(source.func_ode(other_charges))
 
+        def time_step_body(time_idx, s):
+            print(time_idx)
+            jax.debug.print("{x}", x=time_idx)
+            time = ts[time_idx]
+
+            for source_idx, _ in enumerate(sources):
+                current_s = s[source_idx][time_idx]
+                updated_s = rk4_step(func_ode_list[source_idx], time, time + dt, current_s, dt)
+
+                s[source_idx] = s[source_idx].at[time_idx + 1].set(updated_s)
+                s[source_idx] = s[source_idx].at[-1].set(updated_s)
+
+            return s
+
+        final_states_with_buffer = jax.lax.fori_loop(0, t_num - 1, time_step_body, state_list)
+        return [s[:-1] for s in final_states_with_buffer]
+
         # TODO: Define one scan step jax.lax.fori_loop
-        for time_idx, time in enumerate(tqdm(ts[:-1])):
-            # print(time_idx)
-            # jax.debug.print("...{x}", x=time_idx)
-            # print(time)
+        # for time_idx, time in enumerate((ts[:-1])):
+        #     print(time_idx)
+        #     for source_idx, _ in enumerate(sources):
+        #         current_state = state_list[source_idx][time_idx]
+        #         updated_state = rk4_step(func_ode_list[source_idx], time, time + dt, current_state, dt)
 
-            for source_idx, source in enumerate(sources):
-                current_state = state_list[source_idx][time_idx]
-                updated_state = rk4_step(func_ode_list[source_idx], time, time + dt, current_state, dt)
+        #         state_list[source_idx] = state_list[source_idx].at[time_idx + 1].set(updated_state)
+        #         state_list[source_idx] = state_list[source_idx].at[-1].set(updated_state)
 
-                state_list[source_idx] = state_list[source_idx].at[time_idx + 1].set(updated_state)
-                state_list[source_idx] = state_list[source_idx].at[-1].set(updated_state)
-
-        return [s[:-1] for s in state_list]  # Pop last element
+        # return [s[:-1] for s in state_list]  # Pop last element
 
     return simulate_fn
 
@@ -126,13 +139,13 @@ if __name__ == "__main__":
     )
 
     dipole1 = dipole_source(
-        positions_0=[lambda t: jnp.array([0.0, 80e-9, 0.5e-9]), lambda t: jnp.array([0.0, 80e-9, -0.5e-9])],
+        positions_0=[lambda t: jnp.array([0.0, 80e-9, 0.5e-12]), lambda t: jnp.array([0.0, 80e-9, -0.5e-12])],
         q=e,
         omega_0=100e12 * 2 * jnp.pi,
         m=m_e,
     )
 
-    num_steps = 400
+    num_steps = 40_000
     dt = 1e-18
     t_end = (num_steps - 1) * dt
     ts = jnp.linspace(0, t_end, num_steps)
@@ -143,4 +156,7 @@ if __name__ == "__main__":
     state_list = sim_fn(ts)
     plt.plot(state_list[0][:, 0, 0, 2])
     plt.plot(state_list[0][:, 1, 0, 2])
+    plt.show()
+    plt.plot(state_list[1][:, 0, 0, 2])
+    plt.plot(state_list[1][:, 1, 0, 2])
     plt.show()
