fdc4d95defd471600dc2fdccaa457819cc8102b6
Matthew Filipovich <42307495+MatthewFilipovich@users.noreply.github.com>
2025-09-28 21:18:57 -0700

    Fixes

--- pycharge/simulate/interpolate_position.py
+++ pycharge/simulate/interpolate_position.py
@@ -7,7 +7,6 @@ import jax.numpy as jnp
 def interpolate_position(ts, position_0: Callable, position_array, velocity_array, default_nan_position=None):
     t_start = ts[0]
     t_end = ts[-1]
-    dt = ts[1] - ts[0]
     nan_value = default_nan_position if default_nan_position is not None else jnp.full(3, jnp.nan)
 
     def before_start(t):
@@ -17,13 +16,14 @@ def interpolate_position(ts, position_0: Callable, position_array, velocity_arra
         return nan_value
 
     def interpolate(t):
-        t_idx = ((t - t_start) / dt).astype(int)
+        t_idx = jnp.searchsorted(ts, t, side="right") - 1
 
         pos0 = position_array[t_idx]
         vel0 = velocity_array[t_idx]
         pos1 = position_array[t_idx + 1]
         vel1 = velocity_array[t_idx + 1]
 
+        dt = ts[t_idx + 1] - ts[t_idx]
         dpos = pos1 - pos0
 
         a = (vel0 + vel1) * dt - 2 * dpos
--- pycharge/simulate/simulate.py
+++ pycharge/simulate/simulate.py
@@ -7,16 +7,12 @@ from .interpolate_position import interpolate_position
 
 
 def simulate(sources):
-    def simulate_fn(ts):  # TODO: change to ts and allow non-uniform spacing
-        t_num = len(ts)
-        t_start = ts[0]
-        dt = ts[1] - ts[0]
-
+    def simulate_fn(ts):
         def create_initial_state(source):
-            state_array = jnp.full([t_num, len(source.charges_0), 2, 3], jnp.nan)
+            state_array = jnp.full([len(ts), len(source.charges_0), 2, 3], jnp.nan)
 
             for charge_idx, charge in enumerate(source.charges_0):
-                pos0, vel0 = charge.position(t_start), jax.jacobian(charge.position)(t_start)
+                pos0, vel0 = charge.position(ts[0]), jax.jacobian(charge.position)(ts[0])
                 state_array = state_array.at[0, charge_idx].set([pos0, vel0])
             return state_array
 
@@ -35,18 +31,21 @@ def simulate(sources):
 
                 return Charge(position, q)
 
-            jax.debug.print("{x}", x=time_idx)
+            # jax.debug.print("{x}", x=time_idx)
 
             charges = tuple(
                 tuple(create_charge(s_idx, c_idx) for c_idx, _ in enumerate(source.charges_0))
                 for s_idx, source in enumerate(sources)
             )
 
-            time = ts[time_idx]
+            t0 = ts[time_idx]
+            t1 = ts[time_idx + 1]
+            dt = t1 - t0
+
             for source_idx, source in enumerate(sources):
                 other_charges = tuple(ch for i, src in enumerate(charges) if i != source_idx for ch in src)
                 current_s = state[source_idx][time_idx]
-                updated_s = rk4_step(source.ode_func, time, time + dt, current_s, dt, other_charges)
+                updated_s = rk4_step(source.ode_func, t0, t1, current_s, dt, other_charges)
 
                 state = (
                     state[:source_idx]
@@ -57,7 +56,7 @@ def simulate(sources):
             return state
 
         initial_state = tuple(create_initial_state(source) for source in sources)
-        final_state = jax.lax.fori_loop(0, t_num - 1, time_step_body, initial_state)
+        final_state = jax.lax.fori_loop(0, len(ts) - 1, time_step_body, initial_state)
 
         return final_state
 
@@ -73,6 +72,8 @@ def rk4_step(term, t0, t1, y0, dt, other_charges):
 
 
 if __name__ == "__main__":
+    from time import time
+
     import matplotlib.pyplot as plt
     from scipy.constants import e, m_e
 
@@ -85,15 +86,23 @@ if __name__ == "__main__":
         d0=[0.0, 0.0, 1e-9], q=e, omega_0=100e12 * 2 * jnp.pi, m=m_e, origin=[0.0, 80e-9, 0.0]
     )
 
-    num_steps = 40_000
+    t_start = 0.0
+    t_num = 40_000
     dt = 1e-18
-    t_end = (num_steps - 1) * dt
-    ts = jnp.linspace(0, t_end, num_steps)
+    t_stop = (t_num - 1) * dt
+    ts = jnp.linspace(t_start, t_stop, t_num)
 
     sim_fn = simulate([dipole0, dipole1])
     sim_fn = jax.jit(sim_fn)
     # sim_fn = equinox.filter_jit(sim_fn)
+
+    start_time = time()
     state_list = sim_fn(ts)
+    print("Time:", time() - start_time)
+    start_time = time()
+    state_list = sim_fn(ts)
+    print("Time:", time() - start_time)
+
     plt.plot(state_list[0][:, 0, 0, 2])
     plt.plot(state_list[0][:, 1, 0, 2])
     plt.show()
